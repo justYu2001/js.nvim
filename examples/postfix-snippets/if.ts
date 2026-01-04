64d225d962cc65600eb76fd87c59ec0c8525d5fe
justYu2001 <jack4781012@gmail.com>
2026-01-04 09:01:31 +0800

    feat: add if postfix snippet

---
+++ examples/postfix-snippets/if.ts
@@ -0,0 +1,26 @@
+true
+
+// Expected Result:
+// if (true) {
+//
+// }
+
+5 % 2
+
+// Expected Result:
+// if (5 % 2) {
+//
+// }
+
+const longCondition = true;
+const reallyLooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooongCondition = false;
+
+longCondition ||
+reallyLooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooongCondition
+
+
+// Expected Result:
+// if (longCondition ||
+// reallyLooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooongCondition) {
+//   
+// }
