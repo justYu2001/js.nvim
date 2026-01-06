92ea7b5a06c9d2726c90381d0889648cbde6f707
YU Chen <49834964+justYu2001@users.noreply.github.com>
2026-01-06 12:20:34 +0800

    feat: add if postfix snippet (#14)

---
+++ lua/js/snippets/if_postfix.lua
@@ -0,0 +1,42 @@
+local M = {}
+
+function M.get_snippets()
+    local ls = require("luasnip")
+    local hybrid_postfix = require("js.snippets.hybrid_postfix")
+    local d = ls.dynamic_node
+    local sn = ls.snippet_node
+    local i = ls.insert_node
+    local t = ls.text_node
+    local queries = require("js.snippets.queries")
+
+    return {
+        hybrid_postfix({
+            trig = ".if",
+            reparseBuffer = "live",
+            matchTSNode = {
+                query = queries.postfix_expression,
+                query_lang = "javascript",
+            },
+            wordTrig = false,
+        }, {
+            d(1, function(_, parent)
+                local matched = parent.snippet.env.LS_TSMATCH
+
+                if not matched or type(matched) ~= "table" or #matched == 0 then
+                    matched = { "" }
+                end
+
+                return sn(nil, {
+                    t("if ("),
+                    t(matched),
+                    t(") {"),
+                    t({ "", "  " }),
+                    i(1),
+                    t({ "", "}" }),
+                })
+            end, {}),
+        }),
+    }
+end
+
+return M
